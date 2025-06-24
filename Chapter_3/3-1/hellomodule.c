083405ff5ac157bfce3d2781cab979911146a854
Wei Feng Lin <franklin@pcpartner.com>
2025-06-24 16:17:38 +0800

    1. Upload labs of Chapter_3.

---
+++ Chapter_3/3-1/hellomodule.c
@@ -0,0 +1,25 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+
+
+static int __init hello_init(void)
+{
+    printk(KERN_EMERG "[ KERN_EMERG ] Hello Module Init.\n");
+    printk("[ default ] Hello Module Init.\n");
+
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    printk("[ default ] Hello Module Exit.\n");    
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL2");
+MODULE_AUTHOR("Frank Lin");
+MODULE_DESCRIPTION("hello module");
+MODULE_ALIAS("test_module");
---
+++ Chapter_3/3-2/calculation.h
@@ -0,0 +1,9 @@
+#ifndef _CALCULATION_H
+#define _CALCULATION_H
+
+extern int itype;
+
+int my_add(int a, int b);
+int my_sub(int a, int b);
+
+#endif
